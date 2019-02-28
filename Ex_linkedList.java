84851d185a066c424fb4ed242284cedee99e9585
dudri63 <dudri63@naver.com>
2019-02-28 17:02:22 +0900

    2

---
+++ Ex_linkedList.java
@@ -0,0 +1,82 @@
+package io.github.dudri63.dataStructure;
+
+import java.util.Scanner;
+
+public class Ex_linkedList {
+	public static void main(String args[]) {
+		System.out.println("현재 리스트는 공백입니다.");
+		LinkedList L = new LinkedList();
+		for(;;) {
+			System.out.println("원하시는 연산의 번호를 입력하십시오.");
+			System.out.println("1. 삽입, 2. 삭제, 3. 역순으로 변경, 4. 출력, 5. 프로그램 종료");
+			Scanner sc = new Scanner(System.in);
+			String input = sc.next();
+			if(input.equals("1")) {
+				System.out.println("원하는 삽입의 방식을 선택하십시오.");
+				System.out.println("1. 앞으로 삽입, 2. 중간으로 삽입, 3. 뒤로 삽입");
+				Scanner sc1 = new Scanner(System.in);
+				String input1 = sc.next();
+				if(input1.contentEquals("1")) {
+					System.out.println("삽입할 데이터를 입력하십시오");
+					Scanner sc11 = new Scanner(System.in);
+					String input11 = sc11.next();
+					L.insertFirstNode(input11);
+				}
+				else if(input1.contentEquals("2")) {
+					System.out.println("삽입할 위치 앞에 작성하였던 데이터를 입력하십시오");
+					Scanner sc121 = new Scanner(System.in);
+					String input121 = sc121.next();
+					System.out.println("삽입할 데이터를 입력합십시오.");
+					Scanner sc122 = new Scanner(System.in);
+					String input122 = sc122.next();
+					ListNode pre = L.searchNode(input121);
+					if(pre == null) {
+						System.out.println("이전에 입력한 데이터가 아닙니다.");
+					}
+					else {
+						L.insertMiddleNode(pre, input122);
+					}
+				}
+				else if(input1.equals("3")) {
+					System.out.println("삽입할 데이터를 입력하십시오.");
+					Scanner sc13 = new Scanner(System.in);
+					String input13 = sc13.next();
+					L.insertLastNode(input13);
+				}
+			}
+			else if(input.equals("2")) {
+				System.out.println("원하는 삭제의 방식을 선택하십시오.");
+				System.out.println("1. 중간 노드 삭제, 2. 마지막 노드 삭제");
+				Scanner sc2 = new Scanner(System.in);
+				String input2 = sc2.next();
+				if(input2.contentEquals("1")) {
+					System.out.println("삭제할 위치 앞에 작성하였던 데이터를 입력하십시오");
+					Scanner sc21 = new Scanner(System.in);
+					String input21 = sc21.next();
+					ListNode pre = L.searchNode(input21);
+					if(pre == null) {
+						System.out.println("이전에 입력한 데이터가 아닙니다.");
+					}
+					else {
+						L.deleteMiddleNode(pre);
+					}
+				}
+				else if(input2.equals("2")) {
+					L.deleteLastNode();
+				}
+			}
+			else if(input.contentEquals("3")) {
+				System.out.println("리스트를 역순으로 변경하였습니다.");
+				L.reverseList();
+			}
+			else if(input.contentEquals("4")) {
+				System.out.println("현재 리스트 상태는 다음과 같습니다.");
+				L.printLinkedList();
+			}
+			else if(input.equals("5")) {
+				System.out.println("프로그램을 종료합니다.");
+				break;
+			}
+		}		
+	}
+}
---
+++ LinkedList.java
@@ -0,0 +1,82 @@
+package io.github.dudri63.dataStructure;
+
+public class LinkedList {
+	private ListNode head;
+	public LinkedList() {
+		head = null;
+	}
+	public void insertFirstNode(String data) {
+		ListNode newNode = new ListNode(data);
+		newNode.link = head;
+		this.head = newNode;
+	}
+	public void insertMiddleNode(ListNode pre, String data) {
+		ListNode newNode = new ListNode(data);
+		newNode.link = pre.link;
+		pre.link = newNode;
+	}
+	public void insertLastNode(String data) {
+		ListNode newNode = new ListNode(data);
+		if (head == null) {
+			this.head = newNode;
+		}
+		else {
+			ListNode temp = head;
+			while(temp.link != null) temp = temp.link;
+			temp.link = newNode;
+		}
+	}
+	public void deleteMiddleNode(ListNode pre) {
+		ListNode old = pre.link;
+		pre.link = old.link;
+	}
+	public void deleteLastNode() {
+		ListNode pre, temp;
+		if(head == null) return;
+		if(head.link == null) {
+			head = null;
+		}
+		else {
+			pre = head;
+			temp = head.link;
+			while(temp.link != null) {
+				pre = temp;
+				temp = temp.link;
+			}
+			pre.link = null;
+		}
+	}
+	public ListNode searchNode(String data) {
+		ListNode temp = this.head;
+		while(temp != null) {
+			if(data.contentEquals(temp.getData()))
+				return temp;
+			else temp = temp.link;
+		}
+		return temp;
+	}
+	public void reverseList() {
+		ListNode next = head;
+		ListNode current = null;
+		ListNode pre = null;
+		while(next != null) {
+			pre = current;
+			current = next;
+			next = next.link;
+			current.link = pre;
+		}
+		head = current;
+	}
+	public void printLinkedList() {
+		ListNode temp = this.head;
+		System.out.printf("L = (");
+		while(temp != null) {
+			System.out.printf(temp.getData());
+			temp = temp.link;
+			if(temp != null) {
+				System.out.printf(", ");
+			}
+		}
+		System.out.println(")");
+	}
+}
